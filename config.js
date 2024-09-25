b8096144de53677e9007c91252fb30afcadec153
divrawat <divrawat2001@gmail>
2024-09-25 15:00:07 +0530

    s0erfve2gr

---
+++ config.js
@@ -0,0 +1,390 @@
+export const DOMAIN = 'https://ossannewbieadventurermanga.online';
+// export const DOMAIN = 'http://localhost:3000';
+
+export const DOMAIN_NAME = 'ossannewbieadventurermanga.online';
+
+export const IMAGES_SUBDOMAIN = 'https://images.mangachimp.com/the-ossan-newbie-adventurer';
+export const IMAGES_SUBDOMAIN_LOGO = 'https://images.mangachimp.com';
+
+// export const MANGA_DESCRIPTION0 = `<a href="${DOMAIN}"><u><b>Vinland Saga</b></u></a>`;
+
+export const MANGA_DESCRIPTION = `<a href="${DOMAIN}"><u><b>The Ossan Newbie Adventurer manga</b></u></a> follows the story of Rick Gladiator, a 32-year-old man who spent years working as a guild clerk but has now decided to pursue a new path as an adventurer. Though older than most rookies, Rick's determination and two years of intense training with Orichalcum Fist, the strongest party on the continent, have endowed him with remarkable strength and skill. Despite being a beginner in the field, his experience and training set him apart, making his journey as a late-blooming adventurer one filled with excitement and promise.`;
+
+
+
+export const MANGA_NAME = 'The Ossan Newbie Adventurer';
+export const MANGA_AUTHOR = 'Kiraku Kishima';
+export const MANGA_RELEASE = '2018';
+export const MANGA_STATUS = 'Ongoing';
+export const MANGA_ARTIST = 'Fujino Ōmori';
+export const MANGA_STUDIO = 'None';
+export const MANGA_GENRE = 'Action, Adventure, Drama, Shonen, Fantasy, School';
+export const MANGA_TYPE = 'Manga';
+
+export const HEADER_MANGA_DESC = "The Ossan Newbie Adventurer follows the story of Rick Gladiator, a 32-year-old man who spent years working as a guild clerk but has now decided to pursue a new path as an adventurer. Though older than most rookies, Rick's determination and two years of intense training with Orichalcum Fist, the strongest party on the continent, have endowed him with remarkable strength and skill. Despite being a beginner in the field, his experience and training set him apart, making his journey as a late-blooming adventurer one filled with excitement and promise."
+
+export const CHAPTER_PREFIX = 'chapter';
+export const IMAGE_PREFIX = 'images/vinland-saga';
+export const NEXT_PREVIOUS_PREFIX = 'chapter';
+export const URL_PREFIX = 'chapter';
+
+export const AUTHOR_PAGE = `${DOMAIN}/the-ossan-newbie-adventurer-team`;
+export const LOGO_URL = `${DOMAIN}/logo.webp`;
+export const COVER_IMG = `${DOMAIN}/cover1.webp`;
+export const BEHIND_COVER_IMG = "";
+
+
+// Navbar
+export const logo = <img src={`${LOGO_URL}`} alt="Logo" className="h-[75px] w-[70px] rounded-[50%] mr-5 md:ml-0 ml-4 md:pb-2 md:my-0 my-1" />
+
+export const APP_NAME = 'The Ossan Newbie Adventurer';
+export const NavbarName = "The Ossan Newbie Adventurer";
+
+export const APP_DESCRIPTION = `Read ${MANGA_NAME} manga online at ${DOMAIN}.`;
+
+export const DISQUS_SHORTNAME = "my-cms-7";
+
+
+export const MANGA_SUMMARY = [
+    {
+        id: 1,
+        content: "The Ossan Newbie Adventurer revolves around Rick Gladiator, a 32-year-old man who spent most of his life working as a humble guild clerk. Though his life was once characterized by routine and predictability, everything changes when Rick decides to step outside of his comfort zone and pursue his long-held dream of becoming an adventurer. Unlike many adventurers who start young, Rick embarks on this path later in life, making him an unconventional figure in the adventuring world. Despite his age, Rick is far from an ordinary rookie—he possesses extraordinary strength and skill, the result of two years of grueling training with the continent's most formidable group, Orichalcum Fist."
+    },
+    {
+        id: 2,
+        content: "Before this dramatic transformation, Rick lived a quiet and modest life, content to work behind the scenes as a guild clerk. His days were spent handling administrative tasks, interacting with adventurers, and watching their impressive feats from a distance. Though Rick admired their courage and strength, he never imagined himself taking part in such daring adventures. However, everything changed when he had the opportunity to witness Orichalcum Fist, the most powerful party on the continent, in action. Their bravery, skill, and unyielding spirit ignited something in Rick, pushing him to reevaluate his life. Inspired by their strength, he made the bold decision to leave his safe, uneventful life behind and embrace the dangers and thrills of adventuring."
+    },
+    {
+        id: 3,
+        content: "With his mind set on a new path, Rick sought out Orichalcum Fist, hoping to learn from the best. For two years, he trained under their guidance, enduring an intense regimen that pushed him to his limits. Through sheer perseverance and dedication, Rick transformed from an unassuming clerk into a warrior with incredible physical strength and sharp reflexes. His training with Orichalcum Fist made him a force to be reckoned with, though he remained largely unknown in the adventuring world."
+    },
+    {
+        id: 4,
+        content: "As Rick sets out on his journey as a new adventurer, he faces numerous obstacles, not the least of which is the prejudice of those who doubt him because of his age. Many people assume that a man in his thirties is too old to begin a career as an adventurer, especially when most start training at a much younger age. Rick is often underestimated, with people believing that his years spent behind a desk have left him out of shape and incapable of surviving the perils of adventuring. Despite these challenges, Rick’s determination never wavers. He confronts every hardship head-on, proving time and again that age is no barrier to success when combined with dedication, skill, and experience."
+    },
+    {
+        id: 5,
+        content: "Throughout his journey, Rick encounters a wide array of powerful enemies, including fierce monsters, cunning bandits, and competitive rival adventurers. Each battle presents new challenges, but Rick’s quick thinking and strategic approach allow him to turn even the most dangerous situations in his favor. His strength, honed through years of training with Orichalcum Fist, makes him a formidable opponent, while his natural ability to adapt to any circumstance sets him apart from the typical adventurer."
+    },
+
+    {
+        id: 7,
+        content: "As Rick continues to prove his worth, he forges meaningful friendships and alliances with fellow adventurers. Many of these new companions recognize his unique talents and potential, offering their support in his quest to become a respected figure in the adventuring world. However, as his reputation grows, so too does the number of enemies who seek to take advantage of his abilities for their own gain. Rick finds himself navigating not only the dangers of the battlefield but also the complexities of a political landscape filled with those who would exploit him for their own nefarious purposes."
+    },
+    {
+        id: 6,
+        content: "Despite the growing challenges, Rick remains steadfast in his values, refusing to compromise his principles. His journey is not only about survival but about carving out a place for himself in a world that initially rejected him due to his age and background. Through sheer willpower, skill, and the support of his allies, Rick rises to become a respected adventurer, a figure whose strength and integrity earn him the admiration of many and the fear of his enemies. As he faces increasingly dangerous foes and high-stakes battles, Rick’s legend grows, marking him as one of the most unlikely yet inspiring adventurers in the world."
+    },
+    {
+        id: 6,
+        content: "In The Ossan Newbie Adventurer, Rick Gladiator's story serves as a powerful reminder that it’s never too late to pursue one’s dreams. His journey from a quiet clerk to a mighty adventurer is filled with peril, excitement, and triumph, proving that with enough determination, anyone can defy expectations and achieve greatness, no matter where they begin."
+    },
+
+
+];
+
+
+
+
+export const navLinks = [
+    { text: 'Chapter 10', href: `${DOMAIN}/chapter-10` },
+    { text: 'Chapter 20', href: `${DOMAIN}/chapter-20` },
+    { text: 'Chapter 30', href: `${DOMAIN}/chapter-30` },
+];
+
+export const FooterLinks = [
+    { text: 'About', href: `${DOMAIN}/about` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+    { text: 'Disclaimer', href: `${DOMAIN}/disclaimer` },
+    { text: 'Privacy Policy', href: `${DOMAIN}/privacy-policy` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` },
+    { text: 'Sitemap', href: `${DOMAIN}/sitemap.xml` }
+];
+
+
+
+
+
+
+
+export const chaptersData =
+
+
+    [
+        {
+            "chapterNumber": "1",
+            "numImages": 38
+        },
+        {
+            "chapterNumber": "1a",
+            "numImages": 19
+        },
+        {
+            "chapterNumber": "1b",
+            "numImages": 19
+        },
+        {
+            "chapterNumber": "2",
+            "numImages": 24
+        },
+        {
+            "chapterNumber": "3",
+            "numImages": 23
+        },
+        {
+            "chapterNumber": "4",
+            "numImages": 25
+        },
+        {
+            "chapterNumber": "5",
+            "numImages": 24
+        },
+        {
+            "chapterNumber": "6",
+            "numImages": 23
+        },
+        {
+            "chapterNumber": "7",
+            "numImages": 15
+        },
+        {
+            "chapterNumber": "8",
+            "numImages": 25
+        },
+        {
+            "chapterNumber": "9",
+            "numImages": 22
+        },
+        {
+            "chapterNumber": "10",
+            "numImages": 26
+        },
+        {
+            "chapterNumber": "11",
+            "numImages": 23
+        },
+        {
+            "chapterNumber": "12",
+            "numImages": 26
+        },
+        {
+            "chapterNumber": "13",
+            "numImages": 26
+        },
+        {
+            "chapterNumber": "14",
+            "numImages": 27
+        },
+        {
+            "chapterNumber": "15",
+            "numImages": 18
+        },
+        {
+            "chapterNumber": "15a",
+            "numImages": 18
+        },
+        {
+            "chapterNumber": "16",
+            "numImages": 30
+        },
+        {
+            "chapterNumber": "16a",
+            "numImages": 15
+        },
+        {
+            "chapterNumber": "16b",
+            "numImages": 15
+        },
+        {
+            "chapterNumber": "17",
+            "numImages": 31
+        },
+        {
+            "chapterNumber": "18",
+            "numImages": 28
+        },
+        {
+            "chapterNumber": "19",
+            "numImages": 28
+        },
+        {
+            "chapterNumber": "20",
+            "numImages": 33
+        },
+        {
+            "chapterNumber": "21",
+            "numImages": 35
+        },
+        {
+            "chapterNumber": "22",
+            "numImages": 31
+        },
+        {
+            "chapterNumber": "23",
+            "numImages": 31
+        },
+        {
+            "chapterNumber": "24",
+            "numImages": 31
+        },
+        {
+            "chapterNumber": "25",
+            "numImages": 31
+        },
+        {
+            "chapterNumber": "26",
+            "numImages": 31
+        },
+        {
+            "chapterNumber": "27",
+            "numImages": 32
+        },
+        {
+            "chapterNumber": "28",
+            "numImages": 26
+        },
+        {
+            "chapterNumber": "29",
+            "numImages": 34
+        },
+        {
+            "chapterNumber": "30",
+            "numImages": 31
+        },
+        {
+            "chapterNumber": "31",
+            "numImages": 33
+        },
+        {
+            "chapterNumber": "32",
+            "numImages": 33
+        },
+        {
+            "chapterNumber": "33",
+            "numImages": 20
+        },
+        {
+            "chapterNumber": "34",
+            "numImages": 18
+        },
+        {
+            "chapterNumber": "35",
+            "numImages": 25
+        },
+        {
+            "chapterNumber": "36",
+            "numImages": 34
+        },
+        {
+            "chapterNumber": "37",
+            "numImages": 26
+        },
+        {
+            "chapterNumber": "38",
+            "numImages": 32
+        },
+        {
+            "chapterNumber": "39",
+            "numImages": 30
+        },
+        {
+            "chapterNumber": "40",
+            "numImages": 34
+        },
+        {
+            "chapterNumber": "41",
+            "numImages": 30
+        },
+        {
+            "chapterNumber": "42",
+            "numImages": 28
+        },
+        {
+            "chapterNumber": "43",
+            "numImages": 29
+        },
+        {
+            "chapterNumber": "44",
+            "numImages": 30
+        },
+        {
+            "chapterNumber": "45",
+            "numImages": 33
+        },
+        {
+            "chapterNumber": "46",
+            "numImages": 36
+        },
+        {
+            "chapterNumber": "47",
+            "numImages": 27
+        },
+        {
+            "chapterNumber": "48",
+            "numImages": 25
+        },
+        {
+            "chapterNumber": "49",
+            "numImages": 31
+        },
+        {
+            "chapterNumber": "50",
+            "numImages": 26
+        },
+        {
+            "chapterNumber": "51",
+            "numImages": 26
+        },
+        {
+            "chapterNumber": "52",
+            "numImages": 31
+        },
+        {
+            "chapterNumber": "53",
+            "numImages": 25
+        },
+        {
+            "chapterNumber": "54",
+            "numImages": 27
+        },
+        {
+            "chapterNumber": "55",
+            "numImages": 26
+        },
+        {
+            "chapterNumber": "56",
+            "numImages": 31
+        },
+        {
+            "chapterNumber": "57",
+            "numImages": 35
+        }
+    ]
+
+
+
+
+
+export const last5chapters = [
+
+    {
+        "chapterNumber": "53",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "54",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "55",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "56",
+        "numImages": 31
+    },
+    {
+        "chapterNumber": "57",
+        "numImages": 35
+    }
+]
